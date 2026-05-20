48e6757c3f4adade785456d9cfb33b6cf1c68661
edongxy <ean.dailo123@gmail.com>
2026-05-20 23:59:11 +0800

    Updated the mock server to an Express server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // ===== MIDDLEWARE =====
 app.use(cors());          
 app.use(express.json());  
-app.use(express.static('public')); // Serves frontend folder assets safely if placed under a public path
+app.use(express.static('public')); // use this if pinagsama-sama yung mga html file in one folder. "public" is the folder name.
 
 // ===== DATABASE CONNECTION =====
 const pool = new Pool({
